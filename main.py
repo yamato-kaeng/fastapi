15918c2d2382bb338e07b5d6e335be40c87d6585
KAENG <yamato@KAENGs-MacBook-Pro.local>
2020-09-25 22:15:35 +0700

    init ark-server API

---
+++ main.py
@@ -0,0 +1,154 @@
+#Author: Chatchawal Sangkeettrakarn
+#Date: September 20,2020.
+
+from fastapi import FastAPI
+import uvicorn
+import numpy as np
+import re
+import requests
+from bs4 import BeautifulSoup
+from fastapi.responses import PlainTextResponse
+
+app = FastAPI()
+
+def result(res):
+    return {"result":res}
+
+@app.get("/")
+async def main():
+    return 'Hello World'
+
+@app.get("/test")
+async def test():
+    return 'Test Tutorial'
+
+@app.get("/add")
+async def add(a: int = 0, b: int = 0):
+    return a+b
+
+@app.get("/mul")
+async def mul(a: int = 0, b: int = 0):
+    return a*b
+
+
+def tonumlist(li):
+    ls = li.split(',')
+    for i in range(len(ls)):
+        ls[i] = float(ls[i])
+    return list(ls)
+
+@app.get("/asc")
+async def asc(li):
+    ls = tonumlist(li)
+    ls.sort()
+    return ls
+
+@app.get("/desc")
+async def desc(li):
+    ls = tonumlist(li)
+    ls.sort(reverse=True)
+    return ls
+
+@app.get("/sum")
+async def sum(li):
+    ls = tonumlist(li)
+    return np.sum(np.array(ls))
+
+@app.get("/avg")
+async def avg(li):
+    ls = tonumlist(li)
+    return np.average(ls)
+
+@app.get("/mean")
+async def mean(li):
+    ls = tonumlist(li)
+    return np.mean(ls)
+
+@app.get("/max")
+async def max(li):
+    ls = tonumlist(li)
+    return np.amax(ls)
+
+@app.get("/min")
+async def min(li):
+    ls = tonumlist(li)
+    return np.amin(ls)
+
+@app.get("/validation-ctzid")
+async def validation_ctzid(text):
+    if(len(text) != 13):
+        return False
+    
+    sum = 0
+    listdata = list(text)
+    
+    for i in range(12):
+        sum+=int(listdata[i])*(13-i)
+        
+    d13 = sum%11
+            
+    d13 = 1 if d13==0 else 0 if d13==1 else 11-d13
+    
+    if d13==int(listdata[12]):
+        return True
+    else:
+        return False
+
+@app.get("/validation-email")
+async def validation_email(text):  
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    if re.search(regex,text):
+        return True
+    else:
+        return False
+    
+    
+@app.get("/google-search",response_class=PlainTextResponse)
+def google_search(text):
+    
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:81.0) Gecko/20100101 Firefox/81.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1'
+    }
+    url = 'https://www.google.com/search?q=' + str(text)
+    res = requests.get(url, headers = headers)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    
+    t = soup.findAll('div', {'class':"r"})
+    i = 0
+    result = ''
+    for a in t:
+        href = a.a['href']
+        head = a.h3.text
+        result = result + head + '<br>' + href + '<br><br>'
+        i += 1
+        if(i >= 5):
+            break
+    
+    return(result)
+
+@app.get("/ark-servers", response_class = PlainTextResponse)
+def ark_server():
+    
+    res = requests.get('https://www.battlemetrics.com/servers/ark/6663725')
+    soup = BeautifulSoup(res.content, 'html.parser')
+    f = soup.find('div', {'class':"col-md-8"}).findAll('a')
+    t = soup.find('div', {'class':"col-md-8"}).findAll('time')
+
+    text = ''
+    for a in range(0,len(f)):
+        print(f[a].text.strip(), t[a].text.strip())
+        text = text + f[a].text.strip() + ' >> ' + t[a].text.strip() + '<br>'
+    
+    text = 'Active players : ' + str(len(f)) + '<br><br>' + text
+    
+    return(text)
+
+
+if __name__ == '__main__':
+   uvicorn.run(app, host="0.0.0.0", port=80, debug=True) 
