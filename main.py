50cb2264da5e9c18743bae8f995c50e0babb7fdf
KAENG <yamato@KAENGs-MacBook-Pro.local>
2020-10-28 14:56:31 +0700

    init twitter-trends and init requirement.txt

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import requests
 import datetime
 import urllib
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
@@ -169,16 +171,31 @@ def google_search_youtube(text):
 @app.get("/twitter-trends", response_class = PlainTextResponse)
 def twitter_trends():
     # twitter_trends! ==> GetTrends twitter !!
-    res = requests.get('https://twitter.com/i/trends')
-    soup = BeautifulSoup(res.content, 'html.parser')
     
-    stringout = ''
+    option = webdriver.ChromeOptions()
+    option.add_argument('headless')
+    #driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options = option)
+    driver = webdriver.Chrome(options = option)
+    driver.get('https://twitter.com/i/trends')
+
+    time.sleep(2)
+    driver.find_element_by_tag_name('body').send_keys(Keys.END)
+    time.sleep(2)
+    driver.find_element_by_tag_name('body').send_keys(Keys.HOME)
+    time.sleep(2)
+    driver.find_element_by_tag_name('body').send_keys(Keys.END)
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
     listcheck = list()
+    stringout = ''
+
     for a in soup.findAll('div', {'class' :'css-901oao r-hkyrab r-1qd0xha r-a023e6 r-b88u0q r-ad9z0x r-bcqeeo r-vmopo1 r-qvutc0'}):
         if(a.text not in listcheck):
             listcheck.append(a.text)
             stringout = stringout + a.text + '<br>'
-    print(stringout)
+
+    driver.quit()
+
     text = 'Time : ' + str(datetime.datetime.now()) + '<br>' + 'Size : ' + str(len(listcheck)) + '<br>' + stringout
     
     return(text)
